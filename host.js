c0c1dd7acb663722b249bfdad2161422d95d8145
tahasharjeel <63963692+tahasharjeel@users.noreply.github.com>
2020-09-10 01:03:15 +0530

    Add files via upload

---
+++ host.js
@@ -0,0 +1,64 @@
+var modal = document.getElementById("hostJoin");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+let code;
+let codInput;
+let checkCode;
+var name1, name2;
+
+function host() {
+  if (document.getElementById("name").value != "") {
+    document.getElementById("codeInput").style.display = "none";
+    document.getElementById("codeLabel").style.display = "none";
+    name1 = document.getElementById("name").value;
+    code = Math.floor((Math.random() * 1000) + 100);
+    localStorage.setItem("vOneLocalStorage", code);
+    document.getElementById("code").innerHTML = "CODE: " + code;
+    document.getElementById("host").style.display = "none";
+    document.getElementById("join").style.display = "none";
+    document.getElementById("playHost").style.display = "block";
+    firebase.database().ref("turn" + code).set({
+      value: true
+    });
+    firebase.database().ref(code).set({
+      code: code
+    });
+    firebase.database().ref("name1" + code).set({
+      player1: name1
+    });
+  } else {
+    alert("NAME cannot be blank");
+  }
+}
+
+function join() {
+  if (document.getElementById("name").value != "") {
+    name2 = document.getElementById("name").value;
+    codeInput = document.getElementById("codeInput").value;
+    localStorage.setItem("vOneLocalStorage", codeInput);
+    var dbRef = firebase.database().ref(codeInput).child("code");
+    dbRef.on('value', snap => checkCode = snap.val());
+    document.getElementById("playJoin").style.display = "block";
+
+  } else {
+    alert("NAME cannot be blank!");
+  }
+}
+
+function play() {
+  if (codeInput == checkCode) {
+    firebase.database().ref("name2" + codeInput).set({
+      player2: name2
+    });
+    window.location = 'start.html';
+  } else {
+    alert("Game not found!");
+  }
+}
